b5850f1fc860529e2cb977cc4012222d0569b245
asyatupaya <doks.swat1488@gmail.com>
2025-10-09 19:25:22 +0300

    Доделал JS

--- Треугольник/script.js
+++ Треугольник/script.js
@@ -5,11 +5,11 @@ const inp3 = document.querySelector('#inp3')
 
 const calc = () => {
 
-    try {
-        const side1 = parseFloat(inp1.value)
-        const side2 = parseFloat(inp2.value)
-        const side3 = parseFloat(inp3.value)
+    const side1 = parseFloat(inp1.value)
+    const side2 = parseFloat(inp2.value)
+    const side3 = parseFloat(inp3.value)
 
+    try {
         if (isNaN(side1 + side2 + side3)) {
             console.log('Неправильно введены числа')
             return
@@ -17,7 +17,7 @@ const calc = () => {
 
         if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
             title.textContent = 'Это не треугольник'
-        } else if (side1 === side2 === side3) {
+        } else if (side1 === side2 && side2 === side3) {
             title.textContent = 'Это равносторонний треугольник'
         } else if (side1 === side2 || side1 === side3 || side2 === side3) {
             title.textContent = 'Это равнобедренный треугольник'
@@ -28,4 +28,10 @@ const calc = () => {
     } catch(_) {
         title.textContent = 'Ошибка введите адекватные числа'
     }
+}
+
+const input = (elem) => {
+    const num = parseFloat(elem.value.replace(',', '.').replace('-', ''))
+    if (!isNaN(num)) elem.value = num
+    else elem.value = ''
 }
