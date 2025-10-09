66f16ada72abbcf2bc234b13de41d402c83e650c
asyatupaya <doks.swat1488@gmail.com>
2025-10-09 22:31:35 +0300

    Add files via upload

---
+++ 1. Treugolnik/script.js
@@ -0,0 +1,37 @@
+const title = document.querySelector('#title')
+const inp1 = document.querySelector('#inp1')
+const inp2 = document.querySelector('#inp2')
+const inp3 = document.querySelector('#inp3')
+
+const calc = () => {
+
+    const side1 = parseFloat(inp1.value)
+    const side2 = parseFloat(inp2.value)
+    const side3 = parseFloat(inp3.value)
+
+    try {
+        if (isNaN(side1 + side2 + side3)) {
+            console.log('Неправильно введены числа')
+            return
+        }
+
+        if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
+            title.textContent = 'Это не треугольник'
+        } else if (side1 === side2 && side2 === side3) {
+            title.textContent = 'Это равносторонний треугольник'
+        } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+            title.textContent = 'Это равнобедренный треугольник'
+        } else {
+            title.textContent = 'Это разносторонний треугольник'
+        }
+
+    } catch(_) {
+        title.textContent = 'Ошибка введите адекватные числа'
+    }
+}
+
+const input = (elem) => {
+    const num = parseFloat(elem.value.replace(',', '.').replace('-', ''))
+    if (!isNaN(num)) elem.value = num
+    else elem.value = ''
+}
